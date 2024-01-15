3e1991f91f46149e1e16ff09577249b52957d346
‪shehap Samer‬‏ <118848577+Shehapsamer@users.noreply.github.com>
2024-01-15 17:31:13 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,54 @@
+// /////////////////////
+window.addEventListener("scroll", function () {
+    let nav = document.querySelector(".navbar");
+    nav.classList.toggle("fix", window.scrollY > 150);
+}) 
+//////////////////////////
+window.addEventListener("scroll" , function () {
+    let scrollUp = document.querySelector(".scroll_up");
+    scrollUp.classList.toggle("scroll_transition", window.scrollY > 150);
+    scrollUp.classList.toggle("show_scroll", window.scrollY > 200)
+})
+    let scrollUp = document.querySelector(".scroll_up");
+
+scrollUp.addEventListener("click", function () {
+    window.scrollTo({
+        top: 0,
+        behavior:"smooth"
+    })
+})
+
+
+
+var owl = $('#slider_2');
+owl.owlCarousel({
+    items:4, 
+  // items change number for slider display on desktop
+  
+    loop:true,
+    margin:10,
+    navigation: true,
+    autoplay: false,
+    smartSpeed: 800,
+    rtl:true,
+    nav: true,
+    navElement: "dots",
+    navText:[`<i class="fa-solid fa-chevron-right"></i>` , `<i class="fa-solid fa-chevron-left"></i>`],
+    paginationSpeed : 800,
+    dots: false,  
+    dotsEach:true,
+    responsive:{
+        0:{
+            items: 1,
+        },
+        600:{
+            items: 2,
+        },
+        1100:{
+            items: 3,
+        },
+        1200:{
+            items:4
+        }
+    }
+});
